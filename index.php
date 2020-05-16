c1a87f0d39d6171a4151b9c6589a5e66bbd687af
Conor O Reilly <x17101701@student.ncirl.ie>
2020-05-16 14:45:17 +0100

    added styling to search bar

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>Youtube</title>
+    <title>VideoTube</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
