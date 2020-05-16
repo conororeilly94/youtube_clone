a8538704ef272bb804fafebc6184c61ee0bb9f88
Conor O Reilly <x17101701@student.ncirl.ie>
2020-05-16 12:53:13 +0100

    added style to main container and created commonActions.js

--- index.php
+++ index.php
@@ -3,14 +3,19 @@
 
 <head>
     <title>Youtube</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script src="assets/js/commonActions.js"></script>
 </head>
 
 <body>
     <div id="pageContainer">
 
         <div id="mastHeadContainer">
-            hello
+            <button class="navShowHide">Click Me</button>
         </div>
 
         <div id="sideNavContainer">
@@ -18,6 +23,9 @@
         </div>
 
         <div id="mainSectionContainer">
+            <div id="mainContentContainer">
+                hello everyone
+            </div>
 
         </div>
 
