dfd472572bf6e97e535391916e32f33f70a873aa
Conor O Reilly <x17101701@student.ncirl.ie>
2020-05-18 19:23:45 +0100

    retrieved categories from the table

--- upload.php
+++ upload.php
@@ -6,10 +6,18 @@ require_once("includes/classes/VideoDetailsFormProvider.php");
 
 <div class="column">
 
-<?php
-$formProvider = new VideoDetailsFormProvider();
-echo $formProvider->createUploadForm();
-?>
+    <?php
+    $formProvider = new VideoDetailsFormProvider();
+    echo $formProvider->createUploadForm();
+
+    $query = $con->prepare("SELECT * FROM Categories");
+    $query->execute();
+
+    while($row = $query->fetch(PDO::FETCH_ASSOC)) {
+        echo $row["name"];
+    }
+
+    ?>
 
 
 </div>
