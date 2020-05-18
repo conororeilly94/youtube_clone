9453cb972030d0089d7316b19c69dc40a1b8cf9a
Conor O Reilly <x17101701@student.ncirl.ie>
2020-05-18 19:38:22 +0100

    created categories dropdown

--- upload.php
+++ upload.php
@@ -7,15 +7,8 @@ require_once("includes/classes/VideoDetailsFormProvider.php");
 <div class="column">
 
     <?php
-    $formProvider = new VideoDetailsFormProvider();
+    $formProvider = new VideoDetailsFormProvider($con);
     echo $formProvider->createUploadForm();
-
-    $query = $con->prepare("SELECT * FROM Categories");
-    $query->execute();
-
-    while($row = $query->fetch(PDO::FETCH_ASSOC)) {
-        echo $row["name"];
-    }
 
     ?>
 
