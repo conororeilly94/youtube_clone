8e1e6161af9d5e1146b17b3da57cba8114202bea
Conor O Reilly <x17101701@student.ncirl.ie>
2020-05-24 13:08:21 +0100

    added email validation

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -11,6 +11,9 @@ class Account {
 
     public function register($fn, $ln, $un, $em, $em2, $pw, $pw2) {
         $this->validateFirstName($fn);
+        $this->validateLastName($ln);
+        $this->validateUsername($un);
+        $this->validateEmails($em, $em2);
     }
 
     private function validateFirstName($fn) {
@@ -37,6 +40,27 @@ class Account {
 
         if($query->rowCount() != 0) {
             array_push($this->errorArray, Constants::$usernameTaken);         
+        }
+
+    }
+
+    private function validateEmails($em, $em2) {
+        if($em != $em2) {
+            array_push($this->errorArray, Constants::$emailsDoNoMatch);
+            return;
+        }
+
+        if(!filter_var($em, FILTER_VALIDATE_EMAIL)) {
+            array_push($this->errorArray, Constants::$emailInvalid);
+            return;
+        }
+
+        $query = $this->con->prepare("SELECT email FROM users WHERE email=:em");
+        $query->bindParam(":un", $em);
+        $query->execute();
+
+        if($query->rowCount() != 0) {
+            array_push($this->errorArray, Constants::$emailTaken);         
         }
 
     }
