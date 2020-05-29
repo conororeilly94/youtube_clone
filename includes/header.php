b82484487c13279671b4e93009edbef9303f4e80
Conor O Reilly <x17101701@student.ncirl.ie>
2020-05-29 16:53:16 +0100

    not signed in alert

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,9 @@
 <?php 
-require_once("includes/config.php");
-require_once("includes/classes/User.php");
-require_once("includes/classes/Video.php");
+require_once("includes/config.php"); 
+require_once("includes/classes/User.php"); 
+require_once("includes/classes/Video.php"); 
 
-$usernameLoggedIn = isset($_SESSION["userLoggedIn"]) ? $_SESSION["userLoggedIn"] : "";
+$usernameLoggedIn = User::isLoggedIn() ? $_SESSION["userLoggedIn"] : "";
 $userLoggedInObj = new User($con, $usernameLoggedIn);
 ?>
 <!DOCTYPE html>
--- watch.php
+++ watch.php
@@ -1,7 +1,7 @@
 <?php 
-require_once("includes/header.php");
-require_once("includes/classes/VideoPlayer.php");
-require_once("includes/classes/VideoInfoSection.php");
+require_once("includes/header.php"); 
+require_once("includes/classes/VideoPlayer.php"); 
+require_once("includes/classes/VideoInfoSection.php"); 
 
 if(!isset($_GET["id"])) {
     echo "No url passed into page";
@@ -11,7 +11,6 @@ if(!isset($_GET["id"])) {
 $video = new Video($con, $_GET["id"], $userLoggedInObj);
 $video->incrementViews();
 ?>
-
 <script src="assets/js/videoPlayerActions.js"></script>
 
 <div class="watchLeftColumn">
@@ -24,11 +23,14 @@ $video->incrementViews();
     echo $videoPlayer->create();
 ?>
 
+
 </div>
 
 <div class="suggestions">
 
 </div>
+
+
 
 
 <?php require_once("includes/footer.php"); ?>
