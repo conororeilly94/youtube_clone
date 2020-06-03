3db8a94583e7abbe3c193ff337e48e449941b9fa
Conor O Reilly <x17101701@student.ncirl.ie>
2020-06-03 15:17:27 +0100

    created and styled subscribe button. Allowed passing of values into subscribe function

---
+++ ajax/subscribe.php
@@ -0,0 +1,11 @@
+<?php
+require_once("../includes/config.php");
+
+if(isset($_POST['userTo']) && isset($_POST['userFrom'])) {
+    echo "good";
+}
+else {
+    echo "One or more parameters are not passed into subscribe.php the file";
+}
+
+?>
