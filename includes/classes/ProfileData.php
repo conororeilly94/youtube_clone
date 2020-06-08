95a13d73bb80064f9294599e46e48c0e0efd0785
Conor O Reilly <x17101701@student.ncirl.ie>
2020-06-08 11:19:36 +0100

    finished profile page

--- includes/classes/ProfileData.php
+++ includes/classes/ProfileData.php
@@ -8,6 +8,10 @@ class ProfileData {
         $this->profileUserObj = new User($con, $profileUsername);
     }
 
+    public function getProfileUserObj() {
+        return $this->profileUserObj;
+    }
+
     public function getProfileUsername() {
         return $this->profileUserObj->getUsername();
     }
@@ -35,6 +39,43 @@ class ProfileData {
 
     public function getSubscriberCount() {
         return $this->profileUserObj->getSubscriberCount();
+    }
+
+    public function getUsersVideos() {
+        $query = $this->con->prepare("SELECT * FROM videos WHERE uploadedBy=:uploadedBy ORDER BY uploadDate DESC");
+        $query->bindParam(":uploadedBy", $username);
+        $username = $this->getProfileUsername();
+        $query->execute();
+
+        $videos = array();
+        while($row = $query->fetch(PDO::FETCH_ASSOC)) {
+            $videos[] = new Video($this->con, $row, $this->profileUserObj->getUsername());
+        }
+        return $videos;
+    }
+
+    public function getAllUserDetails() {
+        return array(
+            "Name" => $this->getProfileUserFullName(),
+            "Username" => $this->getProfileUsername(),
+            "Subscribers" => $this->getSubscriberCount(),
+            "Total views" => $this->getTotalViews(),
+            "Sign up date" => $this->getSignUpDate()
+        );
+    }
+
+    private function getTotalViews() {
+        $query = $this->con->prepare("SELECT sum(views) FROM videos WHERE uploadedBy=:uploadedBy");
+        $query->bindParam(":uploadedBy", $username);
+        $username = $this->getProfileUsername();
+        $query->execute();
+
+        return $query->fetchColumn();
+    }
+
+    private function getSignUpDate() {
+        $date = $this->profileUserObj->getSignUpDate();
+        return date("F jS, Y", strtotime($date));
     }
 }
 ?>
